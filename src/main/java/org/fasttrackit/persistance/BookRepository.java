09036113bd2c5fd71cb8474c77c5110cda879f11
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2020-01-25 13:12:46 +0200

    Implemented CRUD

--- src/main/java/org/fasttrackit/persistance/BookRepository.java
+++ src/main/java/org/fasttrackit/persistance/BookRepository.java
@@ -1,14 +1,17 @@
 package org.fasttrackit.persistance;
 
+import org.fasttrackit.domain.Book;
 import org.fasttrackit.transfer.CreateNewName;
 import org.fasttrackit.transfer.UpdateName;
 
+import javax.swing.plaf.nimbus.State;
 import javax.swing.text.html.HTMLDocument;
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.ConnectException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookRepository {
 
@@ -29,11 +32,42 @@ public class BookRepository {
         String slq = "UPDATE book SET phone_number = ?, address = ? WHERE id = ?";
 
         try (Connection connection = DatabaseConfiguration.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(slq);){
+             PreparedStatement preparedStatement = connection.prepareStatement(slq);) {
             preparedStatement.setString(1, updateName.getPhone());
             preparedStatement.setString(2, updateName.getAddress());
             preparedStatement.setLong(3, id);
             preparedStatement.executeUpdate();
+        }
+    }
+
+    public void delete(long id) throws SQLException, IOException {
+        String sql = "DELETE FROM book WHERE id = ?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public List<Book> getNames() throws IOException, SQLException {
+        String sql = "SELECT id, name, phone-numbers, address FROM book";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)){
+
+            List<Book> names = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Book book = new Book();
+                book.setId(resultSet.getLong("id"));
+                book.setName(resultSet.getString("name"));
+                book.setPhone(resultSet.getString("phone_number"));
+                book.setAddress(resultSet.getString("address"));
+                names.add(book);
+            }
+            return names;
         }
     }
 }
