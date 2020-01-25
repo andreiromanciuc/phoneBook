1ed10ebda613e931b5ad01448a7edca1971d0c48
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2020-01-25 10:10:46 +0200

    initial project and book tab

---
+++ src/main/java/org/fasttrackit/domain/Book.java
@@ -0,0 +1,50 @@
+package org.fasttrackit.domain;
+
+public class Book {
+    private long id;
+    private String name;
+    private String phone;
+    private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
